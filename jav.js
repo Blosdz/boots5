6c2891c13cb38ff68064ddac2288ed59941764aa
johan <johaba.jb@gmail.com>
2022-08-28 17:09:31 -0500

    instructor-dash&form

---
+++ jav.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia29iYWxkcyIsImEiOiJjbDdhbWp0eHUwb3kzM3FvNG5jM2x0bmdqIn0.417ODoEwvDp0Vap8bZ_rpQ';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.06298,42.35725],
+    zoom:18
+    });
